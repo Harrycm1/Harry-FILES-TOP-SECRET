9f9c98504a3e1eaa0d65272e306da48968c4e5ad
Harry Moore <harrisoncmoore@gmail.com>
2025-06-13 10:42:28 +1200

    Update Temperature Converter.py
    second version

--- Temperature Converter.py
+++ Temperature Converter.py
@@ -7,7 +7,7 @@ root.resizable(0, 0)
 
 style = "Arial 15"
 
-# Function to raise a frame
+#Function to raise a frame
 def show_frame(frame):
     frame.tkraise()
 
@@ -15,18 +15,28 @@ def quit_program():
     print("Goodbye world")
     root.destroy()
 
-# -------- Main container frame ----------
+def calccenttofahr():
+    try:
+        n = float(user_entryfahr.get())
+        centtofahr = n * 9 / 5 + 32
+    #   Pray that it works
+        fahrcalc.config(text = centtofahr)
+    except ValueError:
+        fahrcalc.config(text="Invalid input! Enter a number.")
+    
+
+#Main container frame
 container = Frame(root)
 container.pack(fill="both", expand=True)
 
-# --------- Frame 1: Main Menu ----------
+#Frame 1: Main Menu
 frame_main = Frame(container)
 frame_main.place(relwidth=1, relheight=1)
 
 title_label = Label(frame_main, text="Temperature Converter", font=("Arial", 21, "bold"))
 title_label.pack(pady=10)
 
-# Button row (L and R side-by-side)
+#Button row 
 button_frame = Frame(frame_main)
 button_frame.pack()
 
@@ -36,21 +46,30 @@ btn_cent.pack(side=LEFT, padx=10)
 btn_fahr = Button(button_frame, text="to Fahrenheit", bg="red", font=style, command=lambda: show_frame(frame_fahr))
 btn_fahr.pack(side=LEFT, padx=10)
 
-# --------- Frame 2: To Centigrade ----------
+#Frame 2: To Centigrade
 frame_cent = Frame(container)
 frame_cent.place(relwidth=1, relheight=1)
 
-Label(frame_cent, text="Enter temperature in Fahrenheit", font=("Arial", 18)).pack(pady=20)
+Label(frame_cent, text="Enter temperature in Fahrenheit", font=("Arial", 18, "bold")).pack(pady=20)
+user_entrycent = Entry(frame_cent)
+user_entrycent.pack()
+
 Button(frame_cent, text="Back", command=lambda: show_frame(frame_main)).pack()
 
-# --------- Frame 3: To Fahrenheit ----------
+#Frame 3: To Fahrenheit
 frame_fahr = Frame(container)
 frame_fahr.place(relwidth=1, relheight=1)
 
-Label(frame_fahr, text="Enter temperature in Centigrade", font=("Arial", 18)).pack(pady=20)
+Label(frame_fahr, text="Enter temperature in Centigrade", font=("Arial", 18, "bold")).pack(pady=20)
+user_entryfahr = Entry(frame_fahr)
+user_entryfahr.pack()
+
+fahrcalc = Label(frame_fahr, text = "calculated number goes here")
+fahrcalc.pack()
+calcbutton2 = Button(frame_fahr, text="Calculate", command=calccenttofahr).pack()
 Button(frame_fahr, text="Back", command=lambda: show_frame(frame_main)).pack()
 
-# Show main menu initially
+#Show main menu initially
 show_frame(frame_main)
 
 root.mainloop()
