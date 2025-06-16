38750cd5dcd46825cce2ead419cf2663a1a84eee
Harry Moore <harrisoncmoore@gmail.com>
2025-06-17 09:44:12 +1200

    Got rid of sticky :D

--- Superhero name genertor.py
+++ Superhero name genertor.py
@@ -5,7 +5,7 @@ root = Tk()
 radio_var = StringVar()
 
 #Label
-answer = Label(root, text="", font=("Arial", 12, "bold"))
+answer = Label(root, text="", font=("Arial", 9, "bold"))
 answer.grid(row=10, column=0, columnspan=2, pady=10)
 
 #Combine function
@@ -33,23 +33,23 @@ label = Label(root, text="Choose an adjective...", font=("Arial", 10, "bold"))
 label.grid(row=0, column=0, columnspan=2, pady=10)
 
 option1 = ttk.Radiobutton(root, text="Happy", value="1", variable=radio_var)
-option1.grid(row=1, column=0, sticky=W, padx=10)
+option1.grid(row=1, column=0, padx=10)
 
 option2 = ttk.Radiobutton(root, text="Awesome", value="2", variable=radio_var)
-option2.grid(row=2, column=0, sticky=W, padx=10)
+option2.grid(row=2, column=0, padx=10)
 
 option3 = ttk.Radiobutton(root, text="Outgoing", value="3", variable=radio_var)
-option3.grid(row=3, column=0, sticky=W, padx=10)
+option3.grid(row=3, column=0, padx=10)
 
 option4 = ttk.Radiobutton(root, text="Funky", value="4", variable=radio_var)
-option4.grid(row=4, column=0, sticky=W, padx=10)
+option4.grid(row=4, column=0, padx=10)
 
 #Color Entry
 label2 = Label(root, text="Enter a color", font=("Arial", 10, "bold"))
 label2.grid(row=5, column=0, columnspan=2, pady=10)
 
 textbox = Entry(root)
-textbox.grid(row=6, column=0, sticky=W, padx=10)
+textbox.grid(row=6, column=0, padx=10)
 
 #Animal Dropdown
 label3 = Label(root, text="Pick an animal", font=("Arial", 10, "bold"))
@@ -62,7 +62,7 @@ chosen_animals.set("")
 
 my_combobox = ttk.Combobox(root, textvariable=chosen_animals, state="readonly")
 my_combobox['values'] = animals
-my_combobox.grid(row=8, column=0, sticky=W, padx=10)
+my_combobox.grid(row=8, column=0, padx=10)
 
 #Go Button
 go = Button(root, text="GO!", font="Arial 20", command=radio_call)
