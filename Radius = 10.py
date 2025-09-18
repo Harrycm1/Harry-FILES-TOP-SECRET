977dac109943fac5cb769986c28536de5aea09a9
Harry Moore <harrisoncmoore@gmail.com>
2025-09-18 12:26:48 +1200

    Update Radius = 10.py

--- Radius = 10.py
+++ Radius = 10.py
@@ -1,14 +1,18 @@
 radius = 10
+y = 0
 x = int(radius)
 e = int(radius*-1)
-y = 0
 
-while x <= y:
+
+while x > y:
     e += 2*y + 1
     y += 1
 
     if e >= 0:
         x -= 1
         e -= 2*x - 1
+        print(f"{x, y}")
 
-print(radius)
+    elif e < 0:
+        x -= 0
+        print(f"{x, y}")
